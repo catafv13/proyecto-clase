dabedd75d6ab1aca8c25a351d9a2ffb774e55012
catafv13 <catalina.vidal013@gmail.com>
2025-04-08 11:01:59 -0300

    creacion del producto

--- proyecto/src/app/app.routes.ts
+++ proyecto/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { RedirectCommand, Routes } from '@angular/router';
 import path from 'node:path';
 import { InicioComponent } from './inicio/inicio/inicio.component';
 import { FormularioComponent } from './contacto/formulario/formulario.component';
+import { ProductoComponent } from './producto/producto/producto.component';
 
 export const routes: Routes = [
-    {path:"", redirectTo:"/inicio", pathMatch:"full"},
+    {path:"", redirectTo:"/producto", pathMatch:"full"},
     {path:"Inicio", component:InicioComponent},
-    {path: "Contacto", component:FormularioComponent}
+    {path: "Contacto", component:FormularioComponent},
+    {path: "producto", component:ProductoComponent}
 ];
