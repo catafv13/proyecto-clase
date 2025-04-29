379a6f87ff94c4ba0c42761e417ea97eaa8b019a
catafv13 <catalina.vidal013@gmail.com>
2025-04-29 11:02:46 -0300

    continuacion

--- proyecto/src/app/paginas/producto/producto.component.ts
+++ proyecto/src/app/paginas/producto/producto.component.ts
@@ -15,12 +15,39 @@ export class ProductoComponent {
   Productos: Producto[] = [
     {
     id: 1,
-    nombre: "",
+    nombre: "hoodie russell vintage",
     descripcion: "",
     precio: 100000,
-    imagen: "",
+    imagen: "https://i.pinimg.com/736x/7d/7d/01/7d7d01b921ccca4a9934b8955fbb2609.jpg",
     disponibilidad: true
     }, 
+
+    {
+      id: 2,
+      nombre: "hoodie russell vintage",
+      descripcion: "",
+      precio: 100000,
+      imagen: "https://i.pinimg.com/736x/59/e5/75/59e575d01ddb37e44e2b3974b4d7d323.jpg",
+      disponibilidad: true
+      },
+
+      {
+        id: 3,
+        nombre: "hoodie russell vintage",
+        descripcion: "",
+        precio: 100000,
+        imagen: "https://i.pinimg.com/736x/61/bd/4e/61bd4e8db2d81b3b998987d54ed8c4b4.jpg",
+        disponibilidad: true
+        },
+
+        {
+          id: 4,
+          nombre: "hoodie russell vintage",
+          descripcion: "",
+          precio: 100000,
+          imagen: "https://i.pinimg.com/736x/f2/57/bd/f257bde1412ed1f97a417499914ba19a.jpg",
+          disponibilidad: true
+          },
   ]
   constructor(private carritoService: CarritoService) {}
   //metodo para agregar un producto 
